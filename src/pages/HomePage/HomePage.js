d002920fd3211dc51441b77c08f4c6d1af6fd5e4
ashkanbossani <abossani@hotmail.com>
2022-07-28 12:32:50 -0700

    added video for home page successfully

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 import "./HomePage.scss";
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                <p>Hello</p>
+           <div>
+            <div className="hero-container">
+                <div className="video-container">
+                    <ReactPlayer 
+                    className="react-player"
+                    url="//www.youtube.com/embed/TGan48YE9Us?autoplay=1&mute=1&start=20"
+                    width="100%"
+                    height="100%"
+                    position="relative"
+                    playing={true}
+                    loop={true}
+                    muted={false}
+                    />
+                </div>
             </div>
+
+
+
+
+           </div>
         );
     }
 }
