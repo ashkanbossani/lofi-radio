4e4d688de4b401324ddbe473b2daf9b301ab6510
ashkanbossani <abossani@hotmail.com>
2022-07-29 22:41:27 -0700

    finished styling for title subtitel and button added note animations

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -12,7 +12,7 @@ class HomePage extends React.Component {
                 <div className="video-container">
                     <ReactPlayer 
                         className="react-player"
-                        url="https://www.youtube.com/embed/tNyz-uVfSN0" 
+                        url="www.youtube.com/embed/_ITiwPMUzho"
                         width="100%"
                         height="100%"
                         position="relative"
@@ -27,7 +27,7 @@ class HomePage extends React.Component {
                 <h1 className="title">Vibes</h1>
                 <h2 className="subtitle">A Study Space</h2>
             </div>
-            <div className="bubblecontainer">
+            <div className="bubble-container">
                 <div className="bubble1"></div>
                 <div className="bubble2"></div>
                 <div className="bubble3"></div>
@@ -38,6 +38,9 @@ class HomePage extends React.Component {
                  <div class="note-position-2 note-amination animation-delay-2">
                 &#9833;
                 </div>
+                {/* <div class="note-position-3 note-amination animation-delay-3">
+                &#9834;
+                </div> */}
             </div>
 
               
