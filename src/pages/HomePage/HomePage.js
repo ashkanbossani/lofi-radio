975dc2a202b0af55d0c6769de14671c8b0708a8c
ashkanbossani <abossani@hotmail.com>
2022-07-29 20:45:21 -0700

    added styling for intro background video was selected

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,28 +5,48 @@ import "./HomePage.scss";
 class HomePage extends React.Component {
     render() {
         return (
+        <>
            <div>
             <div className="hero-container">
+                <div className="overlay"></div>
                 <div className="video-container">
                     <ReactPlayer 
-                    className="react-player"
-                    url="//www.youtube.com/embed/TGan48YE9Us?autoplay=1&mute=1&start=20"
-                    width="100%"
-                    height="100%"
-                    position="relative"
-                    playing={true}
-                    loop={true}
-                    muted={false}
+                        className="react-player"
+                        url="https://www.youtube.com/embed/tNyz-uVfSN0" 
+                        width="100%"
+                        height="100%"
+                        position="relative"
+                        playing={true}
+                        loop={true}
+                        muted={true}
                     />
                 </div>
             </div>
+             <div className="title-container">
+                <h1 className="title">Lo-fi Vibes</h1>
+                <h2 className="subtitle">Study Space</h2>
+            </div>
+            <div className="bubblecontainer">
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+            </div>
+            <div className="wrap">
+                <button class="welcome-button">Welcome</button>
+            </div>
+
+              
+                
+                 
 
 
 
 
            </div>
-        );
+        </>
+        )
     }
+
 }
 
 export default HomePage;
