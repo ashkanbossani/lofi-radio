13677de9a66d29de53bf9272684f7c40629aac51
ashkanbossani <abossani@hotmail.com>
2022-08-10 12:16:50 -0700

    refactored css for more sass rules on home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -13,7 +13,7 @@ class HomePage extends React.Component {
       </Helmet>
         <div>
           <div className="hero-container">
-            <div className="overlay"></div>
+            <div className="hero-conatiner__overlay"></div>
             <div className="video-container">
               <ReactPlayer
                 className="react-player"
@@ -28,9 +28,9 @@ class HomePage extends React.Component {
             </div>
           </div>
           <div className="title-container">
-            <h1 className="title">Lo-fi</h1>
-            <h1 className="title">Radio</h1>
-            <h2 className="subtitle">A Study Space</h2>
+            <h1 className="title-container__title">Lo-fi</h1>
+            <h1 className="title-container__title">Radio</h1>
+            <h2 className="title-container__subtitle">A Study Space</h2>
           </div>
           <div className="wrap">
             <Link to="/music">
@@ -40,9 +40,6 @@ class HomePage extends React.Component {
             <div className="note-position-2 note-amination animation-delay-2">
               &#9833;
             </div>
-            {/* <div class="note-position-3 note-amination animation-delay-3">
-                &#9834;
-                </div> */}
           </div>
         </div>
       </>
